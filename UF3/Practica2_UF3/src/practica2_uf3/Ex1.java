768e413e6b160b565d99e58bae209860a3296525
ausias <ausias@h7-43-THINK-2020-21>
2022-02-15 10:18:34 +0100

    Omplim Fitxer

--- UF3/Practica2_UF3/src/practica2_uf3/Ex1.java
+++ UF3/Practica2_UF3/src/practica2_uf3/Ex1.java
@@ -8,13 +8,10 @@ e) Esborrar un client
 f) Llistat de tots els clients*/
 package practica2_uf3;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Scanner;
 
 /**
  *
@@ -23,52 +20,39 @@ import java.util.Scanner;
 public class Ex1 {
 
     public static void main(String[] args) throws IOException {
-        Omplir_File();
+        OmplirFile();
     }
-
-    public static String Omplir_File() throws IOException {
-        //Creem el file 
-        File arxiu = new File("./arxiu.txt");
-        arxiu.createNewFile();
-        Scanner s = new Scanner(System.in);
-        FileReader fr = new FileReader(arxiu);
-        //Fem que el llegeixi
-        BufferedReader br = new BufferedReader(fr);
-        String linea = null; //Creem un string per a que es doni el contingut
-        boolean comprovar = true; //boolean per comprovar
-        do {
-            try {
-                System.out.println("Digues una frase ");
-                linea = br.readLine(); //imprimim el contingu al file
-                linea = s.nextLine(); //el llegim per a poguer saber si es null
-                if (linea.equals("")) { //comprovem si es null
-                    System.out.println("La linea esta buida, el programa s'ha aturat");
-                    fr.close();
-                    comprovar = false;
-                    return linea;
-                    //Si ho es para el programa
-                } else {
-                    comprovar = true;
-                    //Si no ho es segueix i ens ho registra en el arxiu
-                    File_print(linea); //amb el file print
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-
-            }
-        } while (comprovar = true);
-        return linea; //tornem el string al main
-    }
-
-    public static File File_print(String F) throws IOException {
-    File arxiu = new File("./arxiu.txt"); //Cridem al file
-    FileWriter write = new FileWriter(arxiu, true);
-    PrintWriter pw = new PrintWriter(write); //el copiem al arxiu
-    pw.println(F);
-    pw.flush();
-    write.close(); //parem
-    return arxiu;
-    }
-}
     
+    public static FileWriter OmplirFile(){
+        FileWriter arxiu = null;
+        PrintWriter pw = null;
+        try {
+            arxiu = new FileWriter("arxiu.txt");
+            pw = new PrintWriter(arxiu);
+            
+            pw.println("|Posició | Inici | Longitud | Descripció   |");
+            pw.println("|   1    |   1   |    6     |Codi          |");
+            pw.println("|   2    |   7   |   20     |Nom           |");
+            pw.println("|   3    |  27   |   30     |Cognoms       |");
+            pw.println("|   4    |  57   |    8     |Data naix.    |");
+            pw.println("|   5    |  65   |   40     |Adreça postal |");
+            pw.println("|   6    | 105   |   30     |E-mail        |");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+        // Nuevamente aprovechamos el finally para
+        // asegurarnos que se cierra el fichero.
+            if (null != arxiu) {
+                arxiu.close();
+            }
+        } catch (Exception e2) {
+            e2.printStackTrace();
+        }
+        return arxiu;
+    }
+    
+}
+
 
