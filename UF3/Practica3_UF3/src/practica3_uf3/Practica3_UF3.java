8e67bb64f90a6d52ef6bed1951ac9904c3dda9d6
ausias <ausias@h7-43-THINK-2020-21>
2022-02-18 13:45:07 +0100

    Penjar

--- UF3/Practica3_UF3/src/practica3_uf3/Practica3_UF3.java
+++ UF3/Practica3_UF3/src/practica3_uf3/Practica3_UF3.java
@@ -4,34 +4,47 @@ Cal que adjunteu a la resolució de la pràctica una captura de pantalla on es v
 el contingut del fitxer en format hexadecimal.*/
 package practica3_uf3;
 
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
  *
  * @author Ramon
  */
-
 public class Practica3_UF3 {
-    
-    public static void main(String[] args) {
-        Scanner s  = new Scanner (System.in);
+
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
         int n = 1;
         CrearFile();
-        while (n>0){
-        System.out.println("Digues un numero"); 
-        n = s.nextInt();
-        LlegirNumeros(n);
-        if (n == 0 ){
-            System.out.println("El programa a finalitzat");
+        while (n > 0) {
+            System.out.println("Digues un numero");
+            n = s.nextInt();
+            LlegirNumeros(n);
+            if (n == 0) {
+                System.out.println("El programa a finalitzat");
+            }
         }
-        }
     }
-    public static File CrearFile(){
-        
-        return null; 
+
+    public static File CrearFile() {
+       
+        return null;
     }
-    public static int LlegirNumeros(int i){
-        return 0;
-    }
+
+    public static FileWriter LlegirNumeros(int i) throws IOException { 
+        File numeros = new File ();
+        FileOutputStream fos = new FileOutputStream (numero);
+        DataOutputStream dos = new DataOutputStream (fos);
+        long num1 ;
+        os.write(i);
+        os.flush();
+        os.close();
+}
 }
